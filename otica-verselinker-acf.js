a44d6840904b590c7c895f4306fb0cc062974101
Heitor Alves <hasneto@gmail.com>
2026-05-05 16:41:00 -0300

    Adiciona script base do VerseLinker ACF

---
+++ otica-verselinker-acf.js
@@ -0,0 +1,10 @@
+/*
+ * Otica Reformada VerseLinker - ACF
+ * Etapa 1: arquivo base
+ * Este script será responsável por reconhecer referências bíblicas
+ * nas postagens e exibir tooltip com texto bíblico da ACF.
+ */
+
+(function () {
+  console.log("Otica Reformada VerseLinker ACF carregado com sucesso.");
+})();
